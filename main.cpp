0208bdc1e58382c94d84a804c69da1ffd3db3ac9
khalii21 <miratkhalikov007@gmail.com>
2026-09-17 02:01:36 +0300

    async and future impl

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <chrono>
+#include <vector>
+#include <future>
+#include <iostream>
 
 namespace khalikov
 {
@@ -9,9 +12,14 @@ namespace khalikov
   private:
     std::chrono::time_point< std::chrono::system_clock > start_;
   };
+
+  using data_t = std::vector< unsigned long long >;
+  using value_t = data_t::value_type;
+
+  value_t sum(data_t::const_iterator start, data_t::const_iterator end);
 }
 
-khalikov::Clicker::Clicker() {
+khalikov::Clicker::Clicker():
   start_(std::chrono::high_resolution_clock::now())
 {}
 
@@ -23,21 +31,40 @@ double khalikov::Clicker::millisec() const {
   return duration_cast< milliseconds >(t - start_).count();
 }
 
-using data_t = std::vector< unsigned long long >;
-using value_t = data_t::value_type;
+khalikov::value_t khalikov::sum(data_t::const_iterator start, data_t::const_iterator end) {
+  value_t sum = 0;
+  for (; start != end; ++start) {
+    sum += *start;
+  }
+  return sum;
+}
 
-int main()
+int main(int argc, char* argv[])
 {
+  if (argc != 2) {
+    return 1;
+  }
   constexpr size_t size(1'000'000'000);
+  size_t threads = std::stoull(argv[1]);
   double init{0}, total{0};
-  value_t sum{0};
+  khalikov::value_t sum{0};
   {
     khalikov::Clicker cl;
-    data_t values(size, 1);
+    khalikov::data_t values(size, 1);
     init = cl.millisec();
-    for (size_t i = 0; i < values.siez(); ++i) {
-      sum += values[i];
+    std::vector< std::future< khalikov::value_t > > futures;
+    for (size_t i = 0; i < threads; ++i) {
+      auto start = values.cbegin() + i * values.size() / threads;
+      auto end = values.cbegin() + (i + 1) * values.size() / threads;
+      futures.push_back(std::async(std::launch::async, khalikov::sum, start, end));
+    }
+    for (auto& ft : futures) {
+      sum += ft.get();
     }
     total = cl.millisec();
   }
+  std::cout << "threads: " << threads << '\n';
+  std::cout << "sum: " << sum << '\n';
+  std::cout << "init: " << init << "ms\n";
+  std::cout << "total: " << total - init << "ms\n";
 }
